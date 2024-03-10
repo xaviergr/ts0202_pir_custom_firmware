72715fcb7eda84e2d18005198a6c3c06f6eda43c
xaviergr <xaviergr@noreply.github.com>
2024-03-10 20:34:27 +0100

    Add an idle task and power management
    The idle task does nothing for now other than powering down the device.
    
    The power management policy is to power down as soon as the device is
    idle and place it deep sleep mode. Deep sleep mode always results in
    RAM memory being cleared when waking up, but it also reduces the total
    power consumption of the device by around 2uA.
    
    Detecting if the device is idle is a bit tricky. For now check if the
    next timer event is the zigbee poll rate task, which signifies that
    there are no other short term tasks to execute and enter deep sleep
    for 6 hours.

--- src/main.c
+++ src/main.c
@@ -9,8 +9,66 @@
 #include <zb_common.h>
 #include <drivers/drv_nv.h>
 
+#include "button.h"
 #include "clusters.h"
 #include "bdb_init.h"
+
+static void enter_low_power(void)
+{
+	u32 irq_state = drv_disable_irq();
+	u32 sleep_time = OVERRIDE_POLL_RATE_MS;
+	drv_pm_wakeup_src_e wake_src = PM_WAKEUP_SRC_PAD;
+	ev_timer_event_t *next_task = ev_timer_nearestGet();
+	drv_pm_sleep_mode_e sleep_mode = PM_SLEEP_MODE_DEEPSLEEP;
+
+	if(next_task) {
+		/* Override the next event if it is the default poll rate task.
+		 * It means that the device is idle and we can sleep for longer. */
+		if (next_task->period >= DEFAULT_POLL_RATE_MS) {
+			next_task->timeout = 0;
+		} else {
+			/* Normal task. */
+			sleep_mode = PM_SLEEP_MODE_DEEP_WITH_RETENTION;
+			sleep_time = next_task->timeout;
+			wake_src |= PM_WAKEUP_SRC_TIMER;
+		}
+	}
+
+	/* Don't deep sleep when haven't joined the network. */
+	if (zb_isDeviceJoinedNwk() == false) {
+		wake_src = PM_WAKEUP_SRC_PAD | PM_WAKEUP_SRC_TIMER;
+		sleep_mode = PM_SLEEP_MODE_DEEP_WITH_RETENTION;
+	}
+
+	rf_paShutDown();
+	if(sleep_mode == PM_SLEEP_MODE_DEEPSLEEP) {
+		drv_pm_deepSleep_frameCnt_set(ss_outgoingFrameCntGet());
+	}
+
+	if(sleep_time <= PM_NORMAL_SLEEP_MAX) {
+		drv_pm_sleep(sleep_mode, wake_src, sleep_time);
+	} else {
+		drv_pm_longSleep(sleep_mode, wake_src, sleep_time);
+	}
+
+	drv_restore_irq(irq_state);
+}
+
+void main_task(void)
+{
+	check_button_status();
+
+	if(bdb_isIdle() && zb_isDeviceJoinedNwk()) {
+		/* Nothing for now. */
+	}
+
+	if(tl_stackBusy() || !zb_isTaskDone()) {
+		return;
+	}
+
+	apsCleanToStopSecondClock();
+	enter_low_power();
+}
 
 static void stack_init(void)
 {
@@ -35,6 +93,8 @@ static void app_init(bool memory_retained)
 		zb_setPollRate(DEFAULT_POLL_RATE_MS);
 	} else {
 		stack_init();
+
+		ev_on_poll(EV_POLL_IDLE, main_task);
 	}
 }
 
